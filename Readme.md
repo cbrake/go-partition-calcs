f48fb2a04f16a32bba21ded899c1661f8730ced4
Cliff Brake <cbrake@bec-systems.com>
2016-06-28 16:33:23 -0400

    update

--- Readme.md
+++ Readme.md
@@ -17,14 +17,64 @@ Example output:
 
 ```
 Old Partitions
-     Device          Name          Start           Size
-       mtd0       xloader            0x0        0x80000
-       mtd1         uboot        0x80000       0x1c0000
-       mtd2     uboot env       0x240000        0x40000
-       mtd3         linux       0x280000       0xa00000
-       mtd4        rootfs       0xc80000     0x186a0000
-       mtd5          data     0x19320000     0x26ce0000
+  Device       Name       Start     Size(B)  Size(MiB)  Size(Blks)
+    mtd0    xloader         0x0     0x80000        0.5           4
+    mtd1      uboot     0x80000    0x1c0000       1.75          14
+    mtd2  uboot env    0x240000     0x40000       0.25           2
+    mtd3      linux    0x280000    0xa00000         10          80
+    mtd4     rootfs    0xc80000  0x186a0000    390.625        3125
+    mtd5       data  0x19320000  0x26ce0000    620.875        4967
 total size = 0x40000000, 1024MiB
+
+
+
+New Partitions 500MB
+  Device       Name       Start    Size(B)  Size(MiB)  Size(Blks)
+    mtd0    xloader         0x0    0x80000        0.5           4
+    mtd1      uboot     0x80000   0x500000          5          40
+    mtd2  uboot env    0x580000    0x40000       0.25           2
+    mtd3     linux1    0x5c0000  0x1400000         20         160
+    mtd4     linux2   0x19c0000  0x1400000         20         160
+    mtd5    rootfs1   0x2dc0000  0xa000000        160        1280
+    mtd6    rootfs2   0xcdc0000  0xa000000        160        1280
+    mtd7        log  0x16dc0000   0xa00000         10          80
+    mtd8       data  0x177c0000  0x7c40000     124.25         994
+total size = 0x1f400000, 500MiB
+
+size of boot partitions:  45.75 MiB
+
+
+New Partitions 1G
+  Device       Name       Start     Size(B)  Size(MiB)  Size(Blks)
+    mtd0    xloader         0x0     0x80000        0.5           4
+    mtd1      uboot     0x80000    0x500000          5          40
+    mtd2  uboot env    0x580000     0x40000       0.25           2
+    mtd3     linux1    0x5c0000   0x1400000         20         160
+    mtd4     linux2   0x19c0000   0x1400000         20         160
+    mtd5    rootfs1   0x2dc0000   0xfa00000        250        2000
+    mtd6    rootfs2  0x127c0000   0xfa00000        250        2000
+    mtd7        log  0x221c0000   0x3200000         50         400
+    mtd8       data  0x253c0000  0x1ac40000     428.25        3426
+total size = 0x40000000, 1024MiB
+
+size of boot partitions:  45.75 MiB
+
+
+New Partitions 2G
+  Device       Name       Start     Size(B)  Size(MiB)  Size(Blks)
+    mtd0    xloader         0x0     0x80000        0.5           4
+    mtd1      uboot     0x80000    0x500000          5          40
+    mtd2  uboot env    0x580000     0x40000       0.25           2
+    mtd3     linux1    0x5c0000   0x1400000         20         160
+    mtd4     linux2   0x19c0000   0x1400000         20         160
+    mtd5    rootfs1   0x2dc0000  0x1f400000        500        4000
+    mtd6    rootfs2  0x221c0000  0x1f400000        500        4000
+    mtd7        log  0x415c0000   0x6400000        100         800
+    mtd8       data  0x479c0000  0x38640000     902.25        7218
+total size = 0x80000000, 2048MiB
+
+size of boot partitions:  45.75 MiB
+
 ```
 
 
--- go-partition-calcs.go
+++ go-partition-calcs.go
@@ -11,8 +11,8 @@ import (
 const (
 	nandBlockSize = 128 * 1024
 	KiB           = 1024
-	MiB           = 1048576
-	GiB           = 1073741824
+	MiB           = 1024 * 1024
+	GiB           = 1024 * 1024 * 1024
 )
 
 type Partition struct {
@@ -24,7 +24,9 @@ type Partition struct {
 
 func (p Partition) String() string {
 	sizeMiB := float32(p.Size) / (1024 * 1024)
-	return fmt.Sprintf("%v\t%v\t0x%x\t0x%x\t%vMiB\t", p.Device, p.Name, p.Start, p.Size, sizeMiB)
+	sizeBlocks := float32(p.Size) / nandBlockSize
+	return fmt.Sprintf("%v\t%v\t0x%x\t0x%x\t%v\t%v\t",
+		p.Device, p.Name, p.Start, p.Size, sizeMiB, sizeBlocks)
 }
 
 type Partitions []Partition
@@ -32,9 +34,9 @@ type Partitions []Partition
 func (parts Partitions) String() string {
 	w := new(tabwriter.Writer)
 	buf := new(bytes.Buffer)
-	w.Init(buf, 10, 0, 5, ' ', tabwriter.AlignRight)
+	w.Init(buf, 5, 0, 2, ' ', tabwriter.AlignRight)
 
-	fmt.Fprintln(w, "Device\tName\tStart\tSize(B)\tSize(MiB)\t")
+	fmt.Fprintln(w, "Device\tName\tStart\tSize(B)\tSize(MiB)\tSize(Blks)\t")
 
 	for _, p := range parts {
 		fmt.Fprintln(w, p.String())
@@ -104,25 +106,73 @@ func main() {
 
 	fmt.Printf("Old Partitions\n%v\n", old)
 
-	new1g := Partitions{
+	new500m := Partitions{
 		Partition{Name: "xloader", Size: 0x80000},
-		Partition{Name: "uboot", Size: 0x1c0000},
+		Partition{Name: "uboot", Size: 5 * MiB},
 		Partition{Name: "uboot env", Size: 0x40000},
-		Partition{Name: "linux1", Size: 20 * 1024 * 1024},
-		Partition{Name: "linux2", Size: 0xa00000},
-		Partition{Name: "rootfs1", Size: 250 * 1024 * 1024},
-		Partition{Name: "rootfs2", Size: 250 * 1024 * 1024},
+		Partition{Name: "linux1", Size: 20 * MiB},
+		Partition{Name: "linux2", Size: 20 * MiB},
+		Partition{Name: "rootfs1", Size: 160 * MiB},
+		Partition{Name: "rootfs2", Size: 160 * MiB},
+		Partition{Name: "log", Size: 10 * MiB},
 		Partition{Name: "data"},
 	}
 
-	new1g.FillIn(1024*1024*1024, nandBlockSize)
+	new500m.FillIn(500*MiB, nandBlockSize)
 
-	fmt.Println("\n\nNew Partitions\n%v\n", new1g)
+	fmt.Printf("\n\nNew Partitions 500MB\n%v\n", new500m)
 
 	bootSize := uint(0)
+	for i := 0; i < 5; i++ {
+		bootSize += new500m.Get(i).Size
+	}
+
+	fmt.Println("size of boot partitions: ", float32(bootSize)/MiB, "MiB")
+
+	new1g := Partitions{
+		Partition{Name: "xloader", Size: 0x80000},
+		Partition{Name: "uboot", Size: 5 * MiB},
+		Partition{Name: "uboot env", Size: 0x40000},
+		Partition{Name: "linux1", Size: 20 * MiB},
+		Partition{Name: "linux2", Size: 20 * MiB},
+		Partition{Name: "rootfs1", Size: 250 * MiB},
+		Partition{Name: "rootfs2", Size: 250 * MiB},
+		Partition{Name: "log", Size: 50 * MiB},
+		Partition{Name: "data"},
+	}
+
+	new1g.FillIn(GiB, nandBlockSize)
+
+	fmt.Printf("\n\nNew Partitions 1G\n%v\n", new1g)
+
+	bootSize = uint(0)
 	for i := 0; i < 5; i++ {
 		bootSize += new1g.Get(i).Size
 	}
 
-	fmt.Println("size of boot partitions: ", bootSize)
+	fmt.Println("size of boot partitions: ", float32(bootSize)/MiB, "MiB")
+
+	new2g := Partitions{
+		Partition{Name: "xloader", Size: 0x80000},
+		Partition{Name: "uboot", Size: 5 * MiB},
+		Partition{Name: "uboot env", Size: 0x40000},
+		Partition{Name: "linux1", Size: 20 * MiB},
+		Partition{Name: "linux2", Size: 20 * MiB},
+		Partition{Name: "rootfs1", Size: 500 * MiB},
+		Partition{Name: "rootfs2", Size: 500 * MiB},
+		Partition{Name: "log", Size: 100 * MiB},
+		Partition{Name: "data"},
+	}
+
+	new2g.FillIn(2*GiB, nandBlockSize)
+
+	fmt.Printf("\n\nNew Partitions 2G\n%v\n", new2g)
+
+	bootSize = uint(0)
+	for i := 0; i < 5; i++ {
+		bootSize += new2g.Get(i).Size
+	}
+
+	fmt.Println("size of boot partitions: ", float32(bootSize)/MiB, "MiB")
+
 }
